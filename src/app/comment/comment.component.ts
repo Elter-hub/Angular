e3acf5532e5f9e08b9a4461a66b9c889fc5f7a44
ihor <ihor04@gmail.com>
2020-07-08 12:44:19 +0300

    shelved

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Comment1} from '../models/Comment1';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,7 +8,14 @@ import {Comment1} from '../models/Comment1';
   styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent {
+  allCommentsArray: Comment1[];
+  isShorten: boolean = true;
 
-  @Input()
-  comment: Comment1;
+  constructor(private commentService: UserService) {
+    this.commentService.getAllComments().subscribe(value => this.allCommentsArray = value) ;
+  }
+
+  showMore() {
+    this.isShorten = !this.isShorten;
+  }
 }
